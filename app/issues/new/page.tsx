3ba4767594dde9294eefc2e0a2421e7b0d248d22
Nghia Vo <vobanghia12@gmail.com>
2024-04-26 18:03:59 -0400

    Customize Radix UI
    Signed-off-by: Nghia Vo <vobanghia12@gmail.com>

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,7 +7,7 @@ const NewIssuePage = () => {
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder='Title' />
       <TextArea placeholder='Description' />
-      <Button>Submit</Button>
+      <Button>Submit New Issue</Button>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@radix-ui/themes/styles.css';
+import './theme-config.css';
 import './globals.css';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import NavBar from './NavBar';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,11 +20,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <Theme>
+    <html lang='en' className={inter.variable}>
+      <body>
+        <Theme accentColor='violet' radius='small'>
           <NavBar />
           <main className='p-5'>{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
