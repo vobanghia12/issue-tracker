7fef040c7074f477c9fc0390c4f59c3339d91762
Nghia Vo <vobanghia12@gmail.com>
2024-04-26 17:47:08 -0400

    Create New Issue Page
    Signed-off-by: Nghia Vo <vobanghia12@gmail.com>

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 const Issues = () => {
-  return <Button>New Issue</Button>;
+  return (
+    <Button>
+      <Link href='/issues/new'>New Issue</Link>
+    </Button>
+  );
 };
 
 export default Issues;
