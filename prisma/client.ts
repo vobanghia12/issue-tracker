3ab71ffbf0e904b3f0be097840c6b27e873072f1
Nghia Vo <vobanghia12@gmail.com>
2024-04-26 17:07:11 -0400

    Building API to creating issue
    Signed-off-by: Nghia Vo <vobanghia12@gmail.com>

---
+++ prisma/client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+//if null, using prismaClientSignleton . Else, use globalThis.prismaGlobal
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
